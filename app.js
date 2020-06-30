2778736e11aa2f58ab3523319936addef83e45e0
nicholas919 <64128376+nicholas919@users.noreply.github.com>
2020-06-30 13:28:17 +0700

    update

--- app.js
+++ app.js
@@ -2,25 +2,6 @@ db.collection('reply').onSnapshot(snapshot =>{
     let changes = snapshot.docChanges();
     changes.forEach(change =>{
         if(change.type == 'added'){
-            function newSort() {
-            var items = $('.custreply > li').get();
-            items.sort(function(a, b) {
-            var keyA = $(a).text();
-            var keyB = $(b).text();
-
-            if (keyA < keyB) return -1;
-            if (keyA > keyB) return 1;
-            return 0;
-              });
-            var ul = $('.custreply');
-            $.each(items, function(i, li) {
-            ul.append(li);
-            });
-  }
-
-$(document).ready(function() {
-  newSort();
-});
             daftarReply(change.doc);
         } else if (change.type == 'removed'){
             let li = custReply.querySelector('[data-id=' + change.doc.id + ']');
