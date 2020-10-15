cc538f926f674da9c564c98293dbdf79c0c08f9e
nicholas919 <64128376+nicholas919@users.noreply.github.com>
2020-10-15 13:55:31 +0700

    update

--- app.js
+++ app.js
@@ -1,3 +1,14 @@
+
+// setup materialize components
+document.addEventListener('DOMContentLoaded', function() {
+
+  var modals = document.querySelectorAll('.modal');
+  M.Modal.init(modals);
+
+  var items = document.querySelectorAll('.collapsible');
+  M.Collapsible.init(items);
+
+});
 $(document).ready(function() {
 db.collection('reply').onSnapshot(snapshot =>{
     var items = $('.custreply > li').get();
@@ -24,34 +35,63 @@ db.collection('reply').onSnapshot(snapshot =>{
         } else if (change.type == 'removed'){
             let li = custReply.querySelector('[data-id=' + change.doc.id + ']');
             custReply.removeChild(li);
+        } else if (change.type == 'modified'){
+            updateReply(change.doc);
         }
     })
 })
 
 
 
-const custReply = document.querySelector('.custreply');
+const custReply = document.querySelector('#custreply');
+const listModalReply = document.querySelector('#list-modal-reply');
 
 function daftarReply(doc){
   let li = document.createElement('li');
+  let modal = document.createElement('div');
+  modal.setAttribute('id', 'modal-reply' + doc.id);
+  modal.classList.add('modal')
   li.setAttribute('data-id', doc.id);
   let header = doc.data().header;
   let keterangan = doc.data().keterangan;
 
-  li.innerHTML = `<div id="header${doc.id}" class="collapsible-header grey lighten-5 card-panel"> ${header} </div>
+  li.innerHTML = `
+        <div id="header-tampilan${doc.id}" class="collapsible-header grey lighten-5 card-panel"> ${header} </div>
         <button id="copas${doc.id}" class="btn waves-effect waves-light btn-large blue darken-2">Copy</button>
         <button id="hapus${doc.id}" class="hapus btn waves-effect waves-light btn-large red lighten-1">x</button>
-        <div id="keterangan${doc.id}" class="collapsible-body ${doc.id} white"> ${keterangan} </div>
+        <div class="collapsible-body ${doc.id}"> 
+        <div id="keterangan-tampilan${doc.id}">${keterangan}</div> 
+        <div class="konfigurasi"><a id="edit${doc.id}" class="btn waves-effect waves-light btn-medium amber lighten-1 modal-trigger" data-target="modal-reply${doc.id}">Edit</a> <a id="hapusKedua${doc.id}" class="btn waves-effect waves-light btn-medium red lighten-1">Hapus</a></div>
+        </div>
 `
-custReply.appendChild(li);
 
+  modal.innerHTML = `
+    <div class="modal-content">
+      <h4 class="center-align">Mengubah Replies</h4>
+      <form id="reply-form${doc.id}">
+        <div>
+        <label>Header</label>
+          <input value="${header}" type="text" id="header${doc.id}" autocomplete="off" required/>
+        </div>
+        <div>
+        <label>Keterangan</label>
+          <textarea onfocus="auto_grow(this)" id="keterangan${doc.id}" class="materialize-textarea" autocomplete="off" required/>${keterangan.replace(/<br\s*[\/]?>/gi, "&#13;&#10;")}</textarea>
+        </div>
+        <div class="btn red darken-3 z-depth-0 modal-close dismiss">Tutup</div>
+        <button type="submit" class="btn blue darken-3 z-depth-0 simpan" onkeyup="submit();">Simpan</button>
+      </form>
+    </div>
+`
+
+custReply.appendChild(li);
+listModalReply.appendChild(modal);
 
  let copas = document.querySelector("#copas" + doc.id);
  copas.addEventListener('click', function (e) {
     document.querySelector('#header' + doc.id).click();
-    var text = document.querySelector("#keterangan" + doc.id);
-    var selection = window.getSelection();
-    var range = document.createRange();
+    let text = document.querySelector("#keterangan" + doc.id);
+    let selection = window.getSelection();
+    let range = document.createRange();
     range.selectNodeContents(text);
     selection.removeAllRanges();
     selection.addRange(range);
@@ -64,19 +104,43 @@ custReply.appendChild(li);
 let hapus = document.querySelector('#hapus' + doc.id);
 hapus.addEventListener('click', function(e){
     e.stopPropagation();
-    var konfirmasi = confirm('Anda yakin ingin menghapus kalimat ini ?');
-    if(konfirmasi ==true){
-    let id = e.target.parentElement.getAttribute('data-id');
-    db.collection('reply').doc(id).delete();
+    let konfirmasi = confirm('Anda yakin ingin menghapus kalimat ini ?');
+    if(konfirmasi == true){
+    db.collection('reply').doc(doc.id).delete();
     }
   });
 
+let hapusKedua = document.querySelector('#hapusKedua' + doc.id);
+hapusKedua.addEventListener('click', function(e){
+    e.stopPropagation();
+    let konfirmasi = confirm('Anda yakin ingin menghapus kalimat ini ?');
+    if(konfirmasi == true){
+    db.collection('reply').doc(doc.id).delete();
+    }    
+});
 
+  let element = document.querySelector('#modal-reply' + doc.id);
+  let instance = M.Modal.init(element)
 
+  let formReply = document.querySelector('#reply-form' + doc.id);
+  formReply.addEventListener('submit', function(e){
+    e.preventDefault();
+  db.collection('reply').doc(doc.id).update({
+    header : document.querySelector('#header' + doc.id).value,
+    keterangan : document.querySelector('#keterangan' + doc.id).value.replace(/\n\r?/g, '<br/>')
+    }).then(() => {
+      instance.close();
+    })
+  })
 
 }
 
-
+function updateReply(doc){
+    let header = doc.data().header;
+    let keterangan = doc.data().keterangan;
+    document.querySelector('#header-tampilan' + doc.id).innerHTML = header;
+    document.querySelector('#keterangan-tampilan' + doc.id).innerHTML = keterangan;
+}
 
 const createForm = document.querySelector('#reply-form');
 createForm.addEventListener('submit', (e) => {
@@ -91,13 +155,8 @@ createForm.addEventListener('submit', (e) => {
         createForm.reset();
     })
 })
-// setup materialize components
-document.addEventListener('DOMContentLoaded', function() {
 
-  var modals = document.querySelectorAll('.modal');
-  M.Modal.init(modals);
-
-  var items = document.querySelectorAll('.collapsible');
-  M.Collapsible.init(items);
-
-});
+function auto_grow(element){
+    element.style.height = "5px";
+    element.style.height = (element.scrollHeight)+"px";
+}
